2511d95e7af5e12b464af68e2508f69d176d258c
dong50252409 <304887750@qq.com>
2021-07-10 22:22:36 +0800

    完善readme,优化性能

--- src/prime_maze.erl
+++ src/prime_maze.erl
@@ -11,11 +11,10 @@
 -spec create_maze(Start :: grid(), End :: grid(), Width :: non_neg_integer(), High :: non_neg_integer()) -> maze().
 create_maze(Start, End, Width, High) ->
     Maze = list_to_tuple(lists:duplicate(High, list_to_tuple(lists:duplicate(Width, 0)))),
-    Maze1 = erase_wall(Start, Maze),
-    Maze2 = erase_wall(End, Maze1),
     Walls = gb_trees:enter(0, {Width div 2, High div 2}, gb_trees:empty()),
-    Visited = #{},
-    gen_maze(Maze2, Walls, Visited, Width, High).
+    Maze1 = gen_maze(Maze, Walls, Width, High),
+    Maze2 = erase_wall(Start, Maze1),
+    erase_wall(End, Maze2).
 
 %%%=================================================================
 %%% Internal Functions
@@ -37,38 +36,28 @@ erase_wall({X, Y}, Maze) ->
     Col = setelement(X, Row, 1),
     setelement(Y, Maze, Col).
 
-add_walls([{X, Y} = Grid | T], Walls, Maze, Visited) ->
+add_walls([{X, Y} = Grid | T], Walls, Maze) ->
     case element(X, element(Y, Maze)) of
         0 ->
-            {Walls1, Visited1} = add_wall(Grid, Walls, Visited),
-            add_walls(T, Walls1, Maze, Visited1);
+            Walls1 = gb_trees:enter(rand:uniform(), Grid, Walls),
+            add_walls(T, Walls1, Maze);
         1 ->
-            add_walls(T, Walls, Maze, Visited)
+            add_walls(T, Walls, Maze)
     end;
-add_walls([], Walls, _Maze, Visited) ->
-    {Walls, Visited}.
+add_walls([], Walls, _Maze) ->
+    Walls.
 
-add_wall(Grid, Walls, Visited) when is_map_key(Grid, Visited) ->
-    {Walls, Visited};
-add_wall(Grid, Walls, Visited) ->
-    Walls1 = gb_trees:enter(rand:uniform(), Grid, Walls),
-    {Walls1, Visited#{Grid => true}}.
-
-pop_wall(Walls) ->
-    {_, Grid, Walls1} = gb_trees:take_smallest(Walls),
-    {Grid, Walls1}.
-
-gen_maze(Maze, {0, nil}, _Visited, _Width, _High) ->
+gen_maze(Maze, {0, nil}, _Width, _High) ->
     Maze;
-gen_maze(Maze, Walls, Visited, Width, High) ->
-    {Grid, Walls1} = pop_wall(Walls),
+gen_maze(Maze, Walls, Width, High) ->
+    {_, Grid, Walls1} = gb_trees:take_smallest(Walls),
     Directions = get_direction(Grid, Width, High),
     case check_ways(Directions, Maze) of
         Count when Count =< 1 ->
             Maze1 = erase_wall(Grid, Maze),
-            {Walls2, Visited1} = add_walls(Directions, Walls1, Maze1, Visited),
-            gen_maze(Maze1, Walls2, Visited1, Width, High);
+            Walls2 = add_walls(Directions, Walls1, Maze1),
+            gen_maze(Maze1, Walls2, Width, High);
         _Count ->
-            gen_maze(Maze, Walls1, Visited, Width, High)
+            gen_maze(Maze, Walls1, Width, High)
     end.
 
